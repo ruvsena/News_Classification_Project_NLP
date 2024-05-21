8e7021b37d47590f8a454bfded87f3aff702ffa9
ruvsena <96788284+ruvsena@users.noreply.github.com>
2024-05-21 10:20:43 +0300

    Update Untitled12.py

--- Untitled12.py
+++ Untitled12.py
@@ -2,8 +2,8 @@
 # coding: utf-8
 
 # In[1]:
-pip install streamlit
-pip install nltk
+import nltk
+nltk.download()
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import nltk
